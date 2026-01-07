4ad06ef062d2e4598c2d5733c974ec389205d39d
James Schurig <jnschurig@gmail.com>
2026-01-06 22:27:26 -0700

    save

--- lua/ghosttysync/highlight_mapping.lua
+++ lua/ghosttysync/highlight_mapping.lua
@@ -151,7 +151,10 @@ function M.set_lualine_highlight(colors)
 	-- lualine_x_diff_removed_command xxx gui=nocombine guifg=#fc3841 guibg=#4e6a78
 	-- lualine_x_diff_removed_terminal xxx gui=nocombine guifg=#fc3841 guibg=#4e6a78
 	-- lualine_x_diff_removed_inactive xxx gui=nocombine guifg=#fc3841 guibg=#1d262a
-	return lualine_simple, nil
+	for key, _ in pairs(lualine) do
+		print("key: " .. key)
+	end
+	return lualine, nil
 end
 -- Create highlight group mappings from Ghostty colors
 function M.create_highlight_map(colors)
